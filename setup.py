b03b703adc3800c3d394e8d71d7b18789ff975f8
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2016-02-08 23:31:16 +0100

    Bugfix and version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='rkvmloader',
-    version='0.3',
+    version='0.4',
     scripts=["rkvmloader"],
     url="https://github.com/robinvanleeuwen/rkvmloader",
     author="Robin van Leeuwen",
