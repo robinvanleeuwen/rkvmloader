ad43fd879ce62852653c142d41b6dfdda19bf581
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2016-02-08 23:18:52 +0100

    Also stop VM's in sequence + some extra options

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
-setup(name = 'rkvmloader',
-      version = '0.1',
-      scripts = ["rkvmloader"],
-      py_modules = ["docopt", "configparser", "libvirt"],
-      url = "https://github.com/robinvanleeuwen/rkvmloader",
-      author = "Robin van Leeuwen",
-      author_email = "robinvanleeuwen@gmail.com"
+setup(name='rkvmloader',
+    version='0.1',
+    scripts=["rkvmloader"],
+    url="https://github.com/robinvanleeuwen/rkvmloader",
+    author="Robin van Leeuwen",
+    author_email="robinvanleeuwen@gmail.com",
+    install_requires=['libvirt-python==1.3.1',
+                      'docopt==0.6.2',
+                      'ptyprocess==0.5.1']
 )
